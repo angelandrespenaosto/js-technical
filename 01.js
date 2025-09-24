4c5d30bfcdcab5b324ca3ce8e8e1b0067c5eee9f
Angel Peña <angelandrespenaosto@gmail.com>
2025-09-24 12:39:45 +0200

    Fixed name exercise 01 for node

--- 01.js
+++ 01.js
@@ -1,88 +1,84 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+const heroes = {
+    'Iron Man': {
+        realName: 'Tony Stark',
+        powers: ['Advanced technology', 'Flight', 'Genius-level intellect'],
+        team: 'Avengers'
+    },
+    'Spider-Man': {
+        realName: 'Peter Parker',
+        powers: ['Wall-crawling', 'Spider-sense', 'Super agility'],
+        team: 'Avengers'
+    },
+    'Captain America': {
+        realName: 'Steve Rogers',
+        powers: ['Super strength', 'Enhanced endurance', 'Vibranium shield'],
+        team: 'Avengers'
+    },
+    'Thor': {
+        realName: 'Thor Odinson',
+        powers: ['God of Thunder', 'Weather control', 'Mjolnir'],
+        team: 'Avengers'
+    },
+    'Black Widow': {
+        realName: 'Natasha Romanoff',
+        powers: ['Martial arts', 'Espionage', 'Enhanced agility'],
+        team: 'Avengers'
+    },
+    'Hulk': {
+        realName: 'Bruce Banner',
+        powers: ['Super strength', 'Regeneration', 'Durability'],
+        team: 'Avengers'
+    },
+    'Doctor Strange': {
+        realName: 'Stephen Strange',
+        powers: ['Magic', 'Time manipulation', 'Dimensional travel'],
+        team: 'Defenders'
+    },
+    'Wolverine': {
+        realName: 'Logan',
+        powers: ['Regeneration', 'Adamantium claws', 'Enhanced senses'],
+        team: 'X-Men'
+    },
+    'Storm': {
+        realName: 'Ororo Munroe',
+        powers: ['Weather manipulation', 'Flight', 'Energy projection'],
+        team: 'X-Men'
+    },
+    'Scarlet Witch': {
+        realName: 'Wanda Maximoff',
+        powers: ['Reality manipulation', 'Chaos magic', 'Telekinesis'],
+        team: 'Avengers'
+    }
+}
 
-    const heroes = {
-        'Iron Man': {
-            realName: 'Tony Stark',
-            powers: ['Advanced technology', 'Flight', 'Genius-level intellect'],
-            team: 'Avengers'
-        },
-        'Spider-Man': {
-            realName: 'Peter Parker',
-            powers: ['Wall-crawling', 'Spider-sense', 'Super agility'],
-            team: 'Avengers'
-        },
-        'Captain America': {
-            realName: 'Steve Rogers',
-            powers: ['Super strength', 'Enhanced endurance', 'Vibranium shield'],
-            team: 'Avengers'
-        },
-        'Thor': {
-            realName: 'Thor Odinson',
-            powers: ['God of Thunder', 'Weather control', 'Mjolnir'],
-            team: 'Avengers'
-        },
-        'Black Widow': {
-            realName: 'Natasha Romanoff',
-            powers: ['Martial arts', 'Espionage', 'Enhanced agility'],
-            team: 'Avengers'
-        },
-        'Hulk': {
-            realName: 'Bruce Banner',
-            powers: ['Super strength', 'Regeneration', 'Durability'],
-            team: 'Avengers'
-        },
-        'Doctor Strange': {
-            realName: 'Stephen Strange',
-            powers: ['Magic', 'Time manipulation', 'Dimensional travel'],
-            team: 'Defenders'
-        },
-        'Wolverine': {
-            realName: 'Logan',
-            powers: ['Regeneration', 'Adamantium claws', 'Enhanced senses'],
-            team: 'X-Men'
-        },
-        'Storm': {
-            realName: 'Ororo Munroe',
-            powers: ['Weather manipulation', 'Flight', 'Energy projection'],
-            team: 'X-Men'
-        },
-        'Scarlet Witch': {
-            realName: 'Wanda Maximoff',
-            powers: ['Reality manipulation', 'Chaos magic', 'Telekinesis'],
-            team: 'Avengers'
-        }
+
+function displaySuperHeroe( aka ) {
+
+    if ( aka in heroes ) {
+
+        const info = heroes[aka]
+        result = `############# ${aka} #############\nReal name: ${info.realName}\nPowers: ${info.powers.join(", ")}\nTeam: ${info.team}`
+
+    } else {
+        result = `############# ${aka} #############\n${aka} does not exist`
     }
 
+    console.log(result)
 
-    function displaySuperHeroe( aka ) {
+}
 
-        if ( aka in heroes ) {
+displaySuperHeroe( "Scarlet Witch" )
 
-            const info = heroes[aka]
-            result = `############# ${aka} #############\nReal name: ${info.realName}\nPowers: ${info.powers.join(", ")}\nTeam: ${info.team}`
+/* Part 2 */
 
-        } else {
-            result = `############# ${aka} #############\n${aka} does not exist`
-        }
+const displayGroupSuperHeroes = (akas) => {
 
-        console.log(result)
+    akas.forEach(aka => {
+        displaySuperHeroe(aka)
+    });
 
-    }
+}
 
-    displaySuperHeroe( "Scarlet Witch" )
-
-    /* Part 2 */
-
-    const displayGroupSuperHeroes = (akas) => {
-
-        akas.forEach(aka => {
-            displaySuperHeroe(aka)
-        });
-
-    }
-
-    displayGroupSuperHeroes([
-        "Iron Man", "Thor", "Joker"
-    ])
-
-});
+displayGroupSuperHeroes([
+    "Iron Man", "Thor", "Joker"
+])
